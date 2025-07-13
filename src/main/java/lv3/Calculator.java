f54c194bdbfa0498fbbf50f0fb3b38c244f7fd1a
seonrizee <seonrizee@gmail.com>
2025-07-14 01:46:24 +0900

    feat: Level 3 개발을 위한 초기 파일 구성

---
+++ src/main/java/lv3/Calculator.java
@@ -0,0 +1,70 @@
+package lv3;
+
+import java.util.ArrayDeque;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Calculator {
+
+    /**
+     * 연산 결과를 저장하는 컬렉션 Queue
+     */
+    private Queue<Integer> results = new ArrayDeque<>();
+
+    /**
+     * 연산 결과를 반환하는 메서드
+     *
+     * @param first    첫번쨰 입력된 숫자
+     * @param second   두번째 입력된 숫자
+     * @param operator 입력된 연산자
+     * @return 연산 결과
+     */
+    public int calculate(int first, int second, char operator) {
+        int result = 0;
+        switch (operator) {
+
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                if (second == 0) {
+                    throw new ArithmeticException();
+                }
+                result = first / second;
+                break;
+        }
+        results.offer(result);
+        return result;
+    }
+
+    /**
+     * @return
+     */
+    public Optional<Integer> removeFirstResult() {
+        return Optional.ofNullable(results.poll());
+    }
+
+    /**
+     * 연산 결과가 저장된 필드에 접근하는 메서드
+     *
+     * @return
+     */
+    public Queue<Integer> getResults() {
+        return new ArrayDeque<>(results);
+    }
+
+    /**
+     * 연산 결과가 저장된 필드를 수정하는 메서드
+     *
+     * @param results
+     */
+    public void setResults(Queue<Integer> results) {
+        this.results = results;
+    }
+}
